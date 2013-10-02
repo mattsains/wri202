2cb30e39a5c53f9dd2cc5b298ac445100f18b3aa
Matthew Sainsbury <matthew@sainsbury.za.net>
2013-10-02 21:42:49 +0200

    Issue #5 - actually staged the files

---
+++ UI-Skeleton/EditStaffScreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UI_Skeleton
+{
+    public partial class EditStaffScreen : UserControl
+    {
+        private int staffID;
+        public EditStaffScreen(int staffID)
+        {
+            InitializeComponent();
+            this.staffID=staffID;
+            this.txtStaffNum.Text = staffID.ToString();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Program.SwitchTo(Screen.ViewStaff);
+        }
+    }
+}
