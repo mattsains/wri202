6a500e92f89eb46b83b4b0aff24500868c083c55
Douglas Bentley <dee.bent.lee@gmail.com>
2013-10-03 14:34:05 +0200

    little more work on main page

--- UI-Skeleton/MainScreen.Designer.cs
+++ UI-Skeleton/MainScreen.Designer.cs
@@ -30,11 +30,15 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainScreen));
             this.lblWelcome = new System.Windows.Forms.Label();
-            this.lblInstruction = new System.Windows.Forms.Label();
             this.pnlHead = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblHowto = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pnlHead.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblWelcome
@@ -50,19 +54,6 @@
             this.lblWelcome.Text = "Welcome to the Tuckshop Management System";
             this.lblWelcome.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // lblInstruction
-            // 
-            this.lblInstruction.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.lblInstruction.BackColor = System.Drawing.Color.Transparent;
-            this.lblInstruction.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblInstruction.ForeColor = System.Drawing.Color.White;
-            this.lblInstruction.Location = new System.Drawing.Point(121, 62);
-            this.lblInstruction.Name = "lblInstruction";
-            this.lblInstruction.Size = new System.Drawing.Size(514, 24);
-            this.lblInstruction.TabIndex = 1;
-            this.lblInstruction.Text = "Use the menu above to navigate";
-            this.lblInstruction.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
             // pnlHead
             // 
             this.pnlHead.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
@@ -70,40 +61,68 @@
             this.pnlHead.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(57)))), ((int)(((byte)(85)))));
             this.pnlHead.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlHead.BackgroundImage")));
             this.pnlHead.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pnlHead.Controls.Add(this.lblInstruction);
             this.pnlHead.Controls.Add(this.lblWelcome);
-            this.pnlHead.Location = new System.Drawing.Point(-11, 398);
+            this.pnlHead.Location = new System.Drawing.Point(-11, 432);
             this.pnlHead.Name = "pnlHead";
-            this.pnlHead.Size = new System.Drawing.Size(761, 104);
+            this.pnlHead.Size = new System.Drawing.Size(761, 84);
             this.pnlHead.TabIndex = 2;
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(188)))), ((int)(((byte)(199)))), ((int)(((byte)(216)))));
-            this.panel1.Location = new System.Drawing.Point(20, 42);
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.lbl1);
+            this.panel1.Controls.Add(this.lblHowto);
+            this.panel1.Location = new System.Drawing.Point(21, 15);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(333, 328);
+            this.panel1.Size = new System.Drawing.Size(697, 397);
             this.panel1.TabIndex = 3;
             // 
-            // panel2
+            // lblHowto
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(188)))), ((int)(((byte)(199)))), ((int)(((byte)(216)))));
-            this.panel2.Location = new System.Drawing.Point(378, 42);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(333, 328);
-            this.panel2.TabIndex = 4;
+            this.lblHowto.AutoSize = true;
+            this.lblHowto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHowto.Location = new System.Drawing.Point(150, 16);
+            this.lblHowto.Name = "lblHowto";
+            this.lblHowto.Size = new System.Drawing.Size(183, 20);
+            this.lblHowto.TabIndex = 0;
+            this.lblHowto.Text = "How to use this software";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(151, 54);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(328, 13);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "When View/Editing tables, any entry may be double clicked to edit it";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(92, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // MainScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.Controls.Add(this.panel2);
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pnlHead);
             this.Name = "MainScreen";
             this.Size = new System.Drawing.Size(738, 503);
             this.pnlHead.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -111,10 +130,12 @@
         #endregion
 
         private System.Windows.Forms.Label lblWelcome;
-        private System.Windows.Forms.Label lblInstruction;
         private System.Windows.Forms.Panel pnlHead;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lblHowto;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
 
     }
 }
