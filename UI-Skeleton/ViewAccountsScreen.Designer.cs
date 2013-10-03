d543b1824dd7eae1357a789bb6719a50506bdf43
Douglas Bentley <dee.bent.lee@gmail.com>
2013-10-03 12:12:23 +0200

    issue #4 fixed

--- UI-Skeleton/ViewAccountsScreen.Designer.cs
+++ UI-Skeleton/ViewAccountsScreen.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.dgAccounts = new System.Windows.Forms.DataGridView();
+            this.cName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cAmountOwing = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cEmail = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.btnEmail = new System.Windows.Forms.Button();
             this.lblEmailPromt = new System.Windows.Forms.Label();
             this.btnMarkAll = new System.Windows.Forms.Button();
             this.btnUnmarkAll = new System.Windows.Forms.Button();
-            this.cName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.cAmountOwing = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.cEmail = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgAccounts)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,9 +43,10 @@
             // 
             this.dgAccounts.AllowUserToAddRows = false;
             this.dgAccounts.AllowUserToDeleteRows = false;
-            this.dgAccounts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgAccounts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgAccounts.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(188)))), ((int)(((byte)(199)))), ((int)(((byte)(216)))));
             this.dgAccounts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgAccounts.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.cName,
@@ -55,6 +56,23 @@
             this.dgAccounts.Name = "dgAccounts";
             this.dgAccounts.Size = new System.Drawing.Size(433, 227);
             this.dgAccounts.TabIndex = 1;
+            // 
+            // cName
+            // 
+            this.cName.HeaderText = "Name";
+            this.cName.Name = "cName";
+            this.cName.ReadOnly = true;
+            // 
+            // cAmountOwing
+            // 
+            this.cAmountOwing.HeaderText = "Amount Owing";
+            this.cAmountOwing.Name = "cAmountOwing";
+            this.cAmountOwing.ReadOnly = true;
+            // 
+            // cEmail
+            // 
+            this.cEmail.HeaderText = "Email?";
+            this.cEmail.Name = "cEmail";
             // 
             // btnEmail
             // 
@@ -68,14 +86,14 @@
             // 
             // lblEmailPromt
             // 
-            this.lblEmailPromt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblEmailPromt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lblEmailPromt.Location = new System.Drawing.Point(4, 4);
             this.lblEmailPromt.Name = "lblEmailPromt";
             this.lblEmailPromt.Size = new System.Drawing.Size(432, 32);
             this.lblEmailPromt.TabIndex = 3;
             this.lblEmailPromt.Text = "If you would like to email accounts to staff, check the email fields of the accou" +
-                "nts you would like to send and then click Email";
+    "nts you would like to send and then click Email";
             // 
             // btnMarkAll
             // 
@@ -98,23 +116,6 @@
             this.btnUnmarkAll.Text = "Unmark All";
             this.btnUnmarkAll.UseVisualStyleBackColor = true;
             this.btnUnmarkAll.Click += new System.EventHandler(this.btnUnmarkAll_Click);
-            // 
-            // cName
-            // 
-            this.cName.HeaderText = "Name";
-            this.cName.Name = "cName";
-            this.cName.ReadOnly = true;
-            // 
-            // cAmountOwing
-            // 
-            this.cAmountOwing.HeaderText = "Amount Owing";
-            this.cAmountOwing.Name = "cAmountOwing";
-            this.cAmountOwing.ReadOnly = true;
-            // 
-            // cEmail
-            // 
-            this.cEmail.HeaderText = "Email?";
-            this.cEmail.Name = "cEmail";
             // 
             // ViewAccountsScreen
             // 
