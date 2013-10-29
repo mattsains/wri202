4d83b74dcc0799622e0c37d0e74187ff60c6fb58
Douglas Bentley <dee.bent.lee@gmail.com>
2013-10-30 00:12:38 +0200

    worked on adding updating a stock item

--- Tuckshop/Screens/EditStockScreen.Designer.cs
+++ Tuckshop/Screens/EditStockScreen.Designer.cs
@@ -144,6 +144,7 @@
             this.btnSave.TabIndex = 10;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnDelete
             // 
